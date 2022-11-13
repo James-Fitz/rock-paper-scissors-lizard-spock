56be04a41912c5ab3bce4fa813aad355e4299ac8
James Fitzpatrick <james.fitz123@gmail.com>
2022-11-13 12:59:58 +0000

    Add function to return a random computer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,13 @@ let computerChoice;
 
 let playerScore = parseInt(document.getElementById("player-score").innerHTML);
 let computerScore = parseInt(document.getElementById("computer-score").innerHTML);
+
+/** 
+ * Creates an array of all possible choices
+ * Generates a random number between 0 and 4
+ * Return computer choice from the array */
+ function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+    const randomChoice = Math.floor(Math.random() * 5)
+    return choices[randomChoice];
+}
