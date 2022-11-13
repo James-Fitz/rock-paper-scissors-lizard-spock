8746a0f3ae1b1d965a2f517fd2a69138fc73988d
James Fitzpatrick <james.fitz123@gmail.com>
2022-11-13 13:31:19 +0000

    Add swith cases for computer winning outcomes

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ let computerScore = parseInt(document.getElementById("computer-score").innerHTML
  * Creates an array of all possible choices
  * Generates a random number between 0 and 4
  * Return computer choice from the array */
- function getComputerChoice() {
+function getComputerChoice() {
     const choices = ["rock", "paper", "scissors", "lizard", "spock"];
     const randomChoice = Math.floor(Math.random() * 5)
     return choices[randomChoice];
@@ -21,7 +21,7 @@ let computerScore = parseInt(document.getElementById("computer-score").innerHTML
 /**
  * Function to increase player score by one.
  */
- function increasePlayerScore() {
+function increasePlayerScore() {
     ++playerScore;
 }
 
@@ -38,7 +38,7 @@ function increaseComputerScore() {
  * Increases the score of the winner by one.
  * Runs the endGame function once the round result is determined.
  */
- function getResult() {
+function getResult() {
     switch (playerChoice + computerChoice) {
         case 'rockscissors':
         case 'rocklizard':
@@ -52,4 +52,17 @@ function increaseComputerScore() {
         case 'spockrock':
             resultText.innerHTML = 'You Win The Round!';
             increasePlayerScore();
+            break;
+        case 'scissorsrock':
+        case 'lizardrock':
+        case 'rockpaper':
+        case 'spockpaper':
+        case 'paperscissors':
+        case 'lizardscissors':
+        case 'paperlizard':
+        case 'spocklizard':
+        case 'scissorsspock':
+        case 'rockspock':
+            resultText.innerHTML = 'Computer Wins The Round!';
+            increaseComputerScore();
             break;
