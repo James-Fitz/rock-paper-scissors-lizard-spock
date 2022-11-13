193a46161f7069db2eacc459a29f7a50b0d295b3
James Fitzpatrick <james.fitz123@gmail.com>
2022-11-13 12:53:29 +0000

    Add global variables to js file

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+/** Setting gloabal variables */
+const playerChoices = document.getElementsByClassName("choice");
+const resultText = document.getElementById("game-result");
+
+let playerChoice;
+let computerChoice;
+
+let playerScore = document.getElementById("player-score");
+let computerScore = document.getElementById("computer-score");
